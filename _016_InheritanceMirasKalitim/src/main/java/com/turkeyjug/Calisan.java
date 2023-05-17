9b4e2477d109b65c109692f1e96ed1e7d9bd8163
Burak Delice <burakdelice.71@gmail.com>
2023-05-17 16:02:41 +0300

    deneme

--- _016_InheritanceMirasKalitim/src/main/java/com/turkeyjug/Calisan.java
+++ _016_InheritanceMirasKalitim/src/main/java/com/turkeyjug/Calisan.java
@@ -6,5 +6,64 @@ public class Calisan {
     String adi;
     String soyadi;
     float maas;
-    boolean sigorta
+    boolean sigorta;
+
+    public Calisan(long kimlikNo, String adi, String soyadi, float maas, boolean sigorta) {
+        this.kimlikNo = kimlikNo;
+        this.adi = adi;
+        this.soyadi = soyadi;
+        this.maas = maas;
+        this.sigorta = sigorta;
+    }
+
+    public long getKimlikNo() {
+        return kimlikNo;
+    }
+
+    public void setKimlikNo(long kimlikNo) {
+        this.kimlikNo = kimlikNo;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    public String getSoyadi() {
+        return soyadi;
+    }
+
+    public void setSoyadi(String soyadi) {
+        this.soyadi = soyadi;
+    }
+
+    public float getMaas() {
+        return maas;
+    }
+
+    public void setMaas(float maas) {
+        this.maas = maas;
+    }
+
+    public boolean isSigorta() {
+        return sigorta;
+    }
+
+    public void setSigorta(boolean sigorta) {
+        this.sigorta = sigorta;
+    }
+
+    @Override
+    public String toString() {
+        return "Calisan{" +
+                "kimlikNo=" + kimlikNo +
+                ", adi='" + adi + '\'' +
+                ", soyadi='" + soyadi + '\'' +
+                ", maas=" + maas +
+                ", sigorta=" + sigorta +
+                '}';
+    }
 }
